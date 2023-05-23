6d6e6530d6b96f4ad40a1e37355143868088209f
masudrana5 <abdullainfo30@gmail.com>
2023-05-23 22:23:34 +0600

    about section create is done

---
+++ src/components/common/About.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import aboutImage from "../../assets/images/mail-to-sms-compatible-toute-messagerie.svg";
+import { TiChevronRightOutline } from "react-icons/ti";
+
+const About = () => {
+  return (
+    <div className="flex container mx-auto items-center gap-5 h-[70vh]">
+      <img src={aboutImage} className="w-full sm:w-2/5" alt="aboutIamge" />
+      <div>
+        <h4 className="text-blue-500 text-2xl font-bold mb-4">About Me</h4>
+        <h2 className="text-gray-200 text-4xl font-normal uppercase">
+          Why Hire me for your next Projects?
+        </h2>
+        <p className="text-gray-200 text-xl mt-5">I'm 24 years old creative <strong>Web developer</strong> based in <a href="http://"><strong>front-end</strong></a> specializing in User Interface Design and Development. I build clean, appealing, and functional interfaces which comply with the latest web standards🔥</p>
+        <br />
+        <p className="text-gray-200 text-xl">I am currently complete web development from <a href="http://" target="_blank" rel="noopener noreferrer"><strong className="hover:text-blue-600">Programming Hero</strong></a> Course. I'm React web developer passionate about open source.</p>
+        <a href="https://" className="flex items-center bg-gradient-to-tr from-pink-500 to-violet-600 px-10 py-4 rounded-full mt-10 text-2xl font-medium text-white group sm:w-[250px]">
+          Download cv
+          <span>
+            <TiChevronRightOutline className="inline-block ml-1 translate-x-0 group-hover:translate-x-2 transition-all duration-200"></TiChevronRightOutline>
+          </span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react";
 import Banner from "../common/Banner";
 import Header from "../layout/Header";
+import About from "../common/About";
 
 const Home = () => {
-
   return (
     <main>
       {/* Header */}
-      <div
-        className={`bg-[#334155] sticky top-0 z-50`}
-      >
+      <div className={`bg-[#334155] sticky top-0 z-50`}>
         <Header />
       </div>
       {/* Banner */}
-      <div className="bg-[#1F2937] h-[100vh]">
+      <div className="bg-[#1F2937]">
         <Banner />
+      </div>
+      {/* about section */}
+      <div className="bg-[#0D012C]">
+        <About />
       </div>
     </main>
   );
