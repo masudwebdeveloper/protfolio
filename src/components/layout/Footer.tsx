338250dc5a306808a52ec0ff6e633e2ba3b2b582
masudrana5 <abdullainfo30@gmail.com>
2023-06-28 19:59:49 +0600

    footer comment

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,6 +3,8 @@ const Footer = () => {
   const getYear = date.getFullYear();
 
   return (
+
+    // this is the footer
     <div className="container sm:px-32 sm:mx-auto text-center pb-5 sm:pb-10">
       <span className="text-gray-950 dark:text-gray-100 text-md sm:text-xl font-medium">
         &copy; {getYear} All right reserved | Made with 💖 by Masud Rana
